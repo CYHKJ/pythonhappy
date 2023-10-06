4edaa76e4abd3120886a88ffdab4b06dca88dcc4
CYHKJ <xoals41412@gmail.com>
2023-10-06 18:01:30 +0900

    himawariaa

--- hello.py
+++ hello.py
@@ -298,7 +298,7 @@ print(len(list))  """
 
 # 큐 
 
-queque = []
+""" queque = []
 
 queque.append(1)
 queque.append(2)
@@ -312,4 +312,126 @@ print(queque)
 front = queque.pop(0)
 print(front)
 print(queque)
-print(len(queque))
+print(len(queque)) """
+
+""" qlist = []
+
+def enqueue(qlist, data):
+    qlist.append(data)
+    
+def dequeue(qlist):
+    data = qlist[0]
+    del qlist[0]
+    return data
+
+enqueue(qlist, 1)
+print(qlist)
+
+enqueue(qlist, 2)
+print(qlist)
+
+enqueue(qlist, 3)
+print(qlist)
+
+dequeue(qlist)
+print(qlist)
+
+dequeue(qlist)
+print(qlist) """
+
+
+""" import time """
+
+""" arr = [1, 2, 3, 4, 5]
+
+def ret_01(idx):
+    return arr[idx]
+
+start = time.time()
+print(ret_01(2))
+end = time.time()
+print(f"{end - start : 5f} sec") """
+
+""" arr = [1, 2, 3, 4, 5]
+
+def print_elements(arr):
+    for elem in arr:
+        print(elem)
+
+start = time.time()
+print_elements(arr)
+end = time.time()
+
+print(f"{end - start : 5f} sec")
+ """
+""" import time
+
+def binary_search(arr, target):
+    left, right = 0, len(arr) - 1
+    print(left, right)
+    
+    while left <= right:
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            left = mid + 1
+        else:
+            right = mid - 1
+    
+    return -1
+
+my_list = [1, 2, 3, 4, 5]
+
+result = binary_search(my_list, 4)
+if result != -1:
+    print(f"요소가 {result}번째 인덱스에 있습니다.")
+else:
+    print("요소를 찾을 수 없습니다.")
+ """
+
+import time
+def mul_for():
+    for i in range(5):
+        for j in range(5):
+            print(i, j)
+            
+start = time.time()
+mul_for()
+end = time.time()
+print(f"{end - start : 5f} sec") 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
