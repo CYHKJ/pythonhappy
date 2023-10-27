fd17cf87705c306b0aa61ca627776087fc38d00c
CYHKJ <xoals41412@gmail.com>
2023-10-27 17:46:01 +0900

    octo27

---
+++ octo272.py
@@ -0,0 +1,64 @@
+# 속성
+
+""" class Person :
+	name = "python"
+	age = 23
+	number = "01012345678"
+ 
+p = Person()
+print(p.name) 
+print(p.age) 
+print(p.number) 
+
+p1 = Person()
+print(p1.name)
+print(p1.age)
+print(p1.number) """
+
+
+# 메서드, 셀프
+
+""" class Person :
+	name = "python"
+	age = 23
+	number = "01012345678"
+
+	def getIntroduce(self):
+		return f"My name is {self.name}"
+
+p = Person()
+print(p.name) 
+print(p.age) 
+print(p.number) 
+print(p.getIntroduce())
+ """
+
+# 클래스 초기화 
+
+class Person :
+	def __init__(self, name, age, number):
+		self.name = name
+		self.age = age
+		self.number = number
+
+p = Person("hello", 24, "01087654321")
+p1 = Person("he", 21, "0108")
+p2 = Person("hee", 24, "028764321")
+
+print(p.name)
+print(p1.name)
+print(p2.name)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
